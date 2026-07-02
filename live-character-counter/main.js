e24899bebf098460b251562b793bd0ad3ca1776a
AcePeQ <maciej.nojszewski22@gmail.com>
2026-07-02 12:53:44 +0200

    feat: create const min and max constraints of the message and init all important HTML elements.

---
+++ live-character-counter/main.js
@@ -0,0 +1,15 @@
+function init() {
+  const MAX_MESSAGE_LENGTH = 120;
+  const MIN_MESSAGE_LENGTH = 10;
+
+  const inputEl = document.querySelector("#message");
+  const submitBtnEl = document.querySelector("#submit-btn");
+  
+  const feedbackEl = document.querySelector("#message-form");
+  const counterEl = document.querySelector(".counter");
+  const successMessEl = document.querySelector("#success-message");
+
+
+}
+
+init();
