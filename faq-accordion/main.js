c1ffb8b797a5ff2c653618ee7e681bfdd1b1d2d1
AcePeQ <maciej.nojszewski22@gmail.com>
2026-07-04 10:15:15 +0200

    feat: enhance FAQ accordion functionality with toggle logic and icon updates

--- faq-accordion/main.js
+++ faq-accordion/main.js
@@ -1,17 +1,56 @@
+// function init() {
+//   const faqList = document.querySelector("#faq-list")
+
+//   function handleClick(event) {
+//     const currentBtn = event.target.closest(".faq-question");
+
+//     if(!currentBtn) return;
+//     const faqItem = currentBtn.closest(".faq-item");
+
+
+
+//     if(!faqItem) return;
+//     const iconEl = faqItem.querySelector(".faq-icon");
+
+//     faqItem.classList.toggle("open");
+//     const isElementOpen = faqItem.classList.contains("open");
+
+//     iconEl.textContent = isElementOpen ? "-" : "+";
+//   }
+
+
+//   faqList.addEventListener("click", handleClick)
+// }
+
+// init();
+
+
 function init() {
   const faqList = document.querySelector("#faq-list")
 
   function handleClick(event) {
     const currentBtn = event.target.closest(".faq-question");
 
-    if(!currentBtn) return;
+    if (!currentBtn) return;
 
     const faqItem = currentBtn.closest(".faq-item");
-
-    if(!faqItem) return;
-
     const iconEl = faqItem.querySelector(".faq-icon");
 
+    if (faqItem.classList.contains("open")) {
+      faqItem.classList.remove("open");
+      iconEl.textContent = "+";
+      return;
+    }
+
+    const openedElementsList = Array.from(faqList.querySelectorAll(".faq-item.open"));
+    openedElementsList.forEach((item) => {
+      item.classList.remove("open");
+      const iconEl = item.querySelector(".faq-icon");
+      iconEl.textContent = "+";
+    })
+
+    faqItem.classList.add("open");
+    iconEl.textContent = "-";
   }
 
 
