201d1ee0a1f58e9e7e23275174d4062d4b6507e0
Maciek <maciej@dharsa.com>
2026-07-07 12:08:07 +0200

    feat: initialize variables and DOM elements in script.js for quiz functionality

--- quiz/script.js
+++ quiz/script.js
@@ -1,8 +1,22 @@
 import { questions } from "questions.js"
 
 function init() {
+  const scoreEl = document.querySelector("#score");
+  const questionCounterEl = document.querySelector("#question-counter");
+  const questionProgressEl = document.querySelector("#progress-bar");
+  const questionTitleEl = document.querySelector("#question-title");
 
+  const questionBoxEl = document.querySelector("#question-box");
+  const answersListEl = document.querySelector("#answers-list");
+  const feedbackEl = document.querySelector("#feedback");
+  const nextBtnEl = document.querySelector("#next-btn");
+  const resultBoxEl = document.querySelector("#result-box");
+  const resultMessageEl = document.querySelector("#result-message");
+  const restartBtnEl = document.querySelector("#restart-btn");
 
+  let currentQuestionIndex = 0;
+  let score = 0;
+  let hasAnswered = false;
 }
 
 init();
