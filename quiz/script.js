8773fbb3fa9e8a7abb0c358c2a40df5a92ddcf33
AcePeQ <maciej.nojszewski22@gmail.com>
2026-07-08 19:28:25 +0200

    feat: implement quiz logic with answer handling and progress tracking in script.js

--- quiz/script.js
+++ quiz/script.js
@@ -1,4 +1,4 @@
-import { questions } from "questions.js"
+import { questions } from "./questions.js"
 
 function init() {
   const scoreEl = document.querySelector("#score");
@@ -17,6 +17,114 @@ function init() {
   let currentQuestionIndex = 0;
   let score = 0;
   let hasAnswered = false;
+
+  function updateQuestionInfo() {
+    questionCounterEl.textContent = `Pytanie ${currentQuestionIndex + 1} z ${questions.length}`
+  }
+
+  function createAnswerButton(answer) {
+    const button = document.createElement("button");
+    button.classList.add("answer-btn");
+    button.type = "button";
+    button.dataset.id = answer.id;
+    button.textContent = answer.text;
+
+    answersListEl.appendChild(button);
+  }
+
+  function handleAnswerClick(button) {
+    const buttons = answersListEl.querySelectorAll(".answer-btn");
+
+    buttons.forEach(button => button.disabled = true);
+
+    const currentQuestion = getCurrentQuestion();
+    const correctAnswerObj = currentQuestion.answers.find(question => question.correct === true);
+
+    const clickedButtonId = button.dataset.id;
+    const correctAnswerId = correctAnswerObj.id;
+
+    if (clickedButtonId === correctAnswerId) handleCorrectAnswer(button);
+    else handleInCorrectAnswer(button, correctAnswerId);
+
+    nextBtnEl.disabled = false;
+    hasAnswered = true;
+  }
+
+  function handleCorrectAnswer(button) {
+    button.classList.add("correct");
+    feedbackEl.classList.add("correct");
+    feedbackEl.textContent = "Correct! You answered right!"
+
+    score += 10;
+  }
+
+  function handleInCorrectAnswer(button, correctAnswerId) {
+    button.classList.add("incorrect");
+
+    feedbackEl.classList.add("incorrect");
+    feedbackEl.textContent = " Wrong! Maybe next time!"
+
+    const correctButtonAnswer = answersListEl.querySelector(`[data-id="${correctAnswerId}"]`);
+    correctButtonAnswer.classList.add("correct");
+  }
+
+  function resetFeedback() {
+    feedbackEl.textContent = "";
+
+    feedbackEl.classList.remove("correct");
+    feedbackEl.classList.remove("incorrect");
+  }
+
+  function getCurrentQuestion() {
+    return questions[currentQuestionIndex];
+  }
+
+  function renderQuestion() {
+    answersListEl.textContent = "";
+    resetFeedback();
+    updateQuestionInfo();
+    updateProgressBar();
+
+    const currentQuestion = getCurrentQuestion();
+
+    questionTitleEl.textContent = currentQuestion.question;
+    currentQuestion.answers.forEach(createAnswerButton)
+
+    nextBtnEl.disabled = true;
+    hasAnswered = false;
+  }
+
+  function updateProgressBar() {
+    const questionsLength = questions.length;
+    questionProgressEl.style.width = `${(currentQuestionIndex + 1) * (100 / questionsLength)}%`
+  }
+
+  function handleNextQuestion() {
+    if (!hasAnswered) return;
+    currentQuestionIndex++;
+
+    const isLastQuestion = questions.length - 1 === currentQuestionIndex;
+    if (!isLastQuestion) {
+      renderQuestion();
+      resetFeedback();
+      return;
+    }
+
+    console.log("no")
+  }
+
+  answersListEl.addEventListener("click", function handleClick(event) {
+    if (hasAnswered) return;
+
+    const button = event.target.closest(".answer-btn");
+    if (!button) return;
+
+    handleAnswerClick(button);
+  })
+
+  nextBtnEl.addEventListener("click", handleNextQuestion);
+
+  renderQuestion();
 }
 
 init();
